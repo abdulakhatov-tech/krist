a418ff6c926620a566c18846da50f21850e9e67a
abdulakhatov-tech <islomabdulakhatov6975@gmail.com>
2025-03-14 08:50:26 +0500

    chore(ui): integrate shadcn for component styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
 	return (
 		<div>
 			<h1 className="text-blue-400 font-bold">Krist E-Commerce Online Shop</h1>
+			<Button>Login</Button>
 		</div>
 	);
 }
