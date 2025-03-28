4cb1d32878bd35c9b5e4b108dce1f622dd3fc5f6
abdulakhatov-tech <islomabdulakhatov6975@gmail.com>
2025-03-28 14:37:21 +0500

    feat(categories): add categories section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,11 @@
 import HomePageComponent from "@/page-components/home-page-component";
+import { LoadingSpinner } from "@/tools";
+import { Suspense } from "react";
 
 export default function Home() {
-	return <HomePageComponent />;
+	return (
+		<Suspense fallback={<LoadingSpinner />}>
+			<HomePageComponent />
+		</Suspense>
+	);
 }
